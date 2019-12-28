3726cd78b3f0305ba956ab76fc118eeb0cbf17ab
3B1S0N <56300965+3B1S0N@users.noreply.github.com>
2019-12-28 10:42:14 +0000

    Update Vehicle.java

--- Data Structures HW2/DS_Homework_2(Vehicle Array)/src/Main/Java/transport/Vehicle.java
+++ Data Structures HW2/DS_Homework_2(Vehicle Array)/src/Main/Java/transport/Vehicle.java
@@ -1,4 +1,18 @@
 package transport;
+/*
+ * File: Vehicle.java
+ * Project: transport
+ * Author: Tobias Woode>
+ * Email: toby2.0@outlook.com
+ * Version: 1.0
+ * Brief: A class created to mimic the features of a generic Vehicle from whichh vehicle type classes will inherit from
+ * -----
+ * Last Modified: Saturday 28th December 2019, 10:36 am
+ * Modified By: Tobias Woode>
+ * -----
+ * Copyright ©2019 Tobias Woode>
+ */
+
 
 public class Vehicle{
 
@@ -47,7 +61,8 @@ public class Vehicle{
         }
         return false;
     }
-
+    
+// Checks if the given vehicle type contains at a driver with a boolean funtion
     public boolean hasDriver() {
         if (this.driver != null){
             return true;
@@ -57,14 +72,16 @@ public class Vehicle{
         }
 
     }
-
+    
+// Checks if the given vehicle type contains at least one passenger with a boolean funtion
     public boolean hasPassengers() {
         if (numberOfPassengers > 0){
             return true;
         }
         return false;
     }
-
+    
+// Checks if the given vehicle type is empty with a boolean funtion
     public boolean isEmpty() {
         if (numberOfPassengers >0 || this.driver != null){
             return false;
@@ -76,6 +93,7 @@ public class Vehicle{
 
     }
 
+ // Checks if the given vehicle type is full with a boolean funtion
     public boolean isFull() {
         if (numberOfPassengers == 4 && this.driver != null){
             return true;
@@ -83,7 +101,10 @@ public class Vehicle{
         return false;
 
     }
-
+    
+// Adds passengers from the beginning (i.e Fills front seating before back seats)
+// Does not exceed the total capacity of the given vehicle type
+    
     public boolean addPassenger(Person newPassenger) {
         if (numberOfPassengers != capacity ) {
             for (int i = 0; i < capacity; i++) {
@@ -97,6 +118,8 @@ public class Vehicle{
         return false;
     }
 
+// Uses a for loop to find and remove passengers from the designated vehicle
+// Has a base case for if the first passenger is not null (i.e if at least one person is in the designated vehicle)
     public void removePassenger(String passengername) {
         for (int i = 0; i < capacity ; i++) {
             if(passengers[1] != null){
